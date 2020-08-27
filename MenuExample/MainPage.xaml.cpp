4de6103a5ecc5986a2e9cf7c93a77aecd3ab96c5
unknown <lalotahoma@icloud.com>
2020-08-27 14:30:19 -0700

    Add MenuExample C++/CX WinUI2.4.3

---
+++ MenuExample/MainPage.xaml.cpp
@@ -0,0 +1,72 @@
+﻿//
+// MainPage.xaml.cpp
+// Implementation of the MainPage class.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+using namespace MenuExample;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+void MainPage::AddItemsToMenu()
+{
+    auto fi = ref new FontIcon();
+
+    auto txtContent = ref new TextBlock();
+    txtContent->TextWrapping = TextWrapping::WrapWholeWords;
+    txtContent->Text = L"Home";
+
+    auto nvi = ref new Microsoft::UI::Xaml::Controls::NavigationViewItem();
+    nvi->Content = txtContent;
+    nvi->Icon = fi;
+    nvi->IsSelected = true;
+
+    this->MenuCollection->Append(nvi);
+    this->navView->SelectedItem = nvi;
+}
+
+template <typename Func>
+Windows::Foundation::IAsyncAction^ MainPage::CallOnUIThread(Func fn)
+{
+    return this->Dispatcher->RunAsync(Windows::UI::Core::CoreDispatcherPriority::Normal,
+        ref new Windows::UI::Core::DispatchedHandler([this, fn]() {
+        fn();
+    }));
+}
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+	m_menuCollection = ref new Platform::Collections::Vector<Microsoft::UI::Xaml::Controls::NavigationViewItem^>();
+    auto waitTask = concurrency::create_task([](){
+        return concurrency::create_async([]() {
+            Sleep(1000);
+        });
+    });
+    //1. This works 100% of the times
+    this->AddItemsToMenu();
+
+    //2. This will work sometimes!
+    //CallOnUIThread([this]() {
+    //    this->AddItemsToMenu();
+    //});
+
+    //3. This never shows the selected item in the nav menu
+    //waitTask.then([this]()
+    //{        
+    //    this->AddItemsToMenu();
+    //});
+}
---
+++ MenuExample/MainPage.xaml.h
@@ -0,0 +1,34 @@
+﻿//
+// MainPage.xaml.h
+// Declaration of the MainPage class.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace MenuExample
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public ref class MainPage sealed
+	{
+	public:
+		void AddItemsToMenu();
+		MainPage();
+		property Windows::Foundation::Collections::IObservableVector<Microsoft::UI::Xaml::Controls::NavigationViewItem^>^ MenuCollection
+		{
+			Windows::Foundation::Collections::IObservableVector<Microsoft::UI::Xaml::Controls::NavigationViewItem^>^ get()
+			{
+				return m_menuCollection;
+			}
+		}
+	private:
+		Platform::Collections::Vector<Microsoft::UI::Xaml::Controls::NavigationViewItem^>^ m_menuCollection = nullptr;
+
+		template<typename Func>
+		Windows::Foundation::IAsyncAction^ CallOnUIThread(Func fn);
+
+	};
+}
